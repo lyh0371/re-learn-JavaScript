198151725f51207ec26688b846896405e3496e0b
unknown <1347635797@qq.com>
2020-09-02 01:14:08 +0800

    shak函数实现

---
+++ JavaScript/shakeProof.js
@@ -0,0 +1,27 @@
+function shakeproof(fn, time) {
+  let flag = true;
+  let timer;
+  return function (...arg) {
+    if (!flag) return false;
+    flag = false;
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...arg);
+      flag = true;
+    }, time);
+  };
+}
+
+// 使用
+
+function test(arg) {
+  console.log(arg);
+}
+
+const Test = shakeproof(test, 2000);
+
+// 模拟点击事件
+setInterval(() => {
+  console.log("我执行了");
+  Test("hah");
+}, 500);
