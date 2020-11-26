9c15cb5368e8106c496172016e586bf450963dec
lyh0371 <1347635797@qq.com>
2020-11-26 09:33:51 +0800

    二叉搜索树

--- JavaScript/二叉搜索树.js
+++ JavaScript/二叉搜索树.js
@@ -6,7 +6,7 @@
  * 整个树中, 最小值在left,最大值在right
  */
 
-function Shu() {
+function searchTree() {
   // 树的节点
   function Node(data) {
     this.key = data;
@@ -16,7 +16,7 @@ function Shu() {
 
   this.root = null;
 
-  Shu.prototype.inster = function (key) {
+  searchTree.prototype.inster = function (key) {
     const newNode = new Node(key);
 
     if (this.root === null) {
@@ -26,7 +26,7 @@ function Shu() {
     }
   };
 
-  Sub.prototype.insterNode = function (node, newNode) {
+  searchTree.prototype.insterNode = function (node, newNode) {
     // todo 递归查找当前数据应该在的位置
 
     if (newNode.key > node.key) {
@@ -46,4 +46,174 @@ function Shu() {
       }
     }
   };
+  // 前序遍历
+  searchTree.prototype.Preorder = function (handler) {
+    this.preOrderNode(this.root, handler);
+  };
+  // 中序遍历
+  searchTree.prototype.Middle = function (handler) {
+    this.preOrderNode(this.root, handler);
+  };
+  // 后序遍历
+  searchTree.prototype.After = function (handler) {
+    this.preOrderNode(this.root, handler);
+  };
+
+  //寻找最大值
+  searchTree.prototype.max = function () {
+    //1.获取根节点
+    let node = this.root;
+    //2.定义key保存节点值
+    let key = null;
+    //3.依次向右不断查找，直到节点为null
+    while (node != null) {
+      key = node.key;
+      node = node.right;
+    }
+    return key;
+  };
+
+  //寻找最小值
+  searchTree.prototype.min = function () {
+    //1.获取根节点
+    let node = this.root;
+    //2.定义key保存节点值
+    let key = null;
+    //3.依次向左不断查找，直到节点为null
+    while (node != null) {
+      key = node.key;
+      node = node.left;
+    }
+    return key;
+  };
+  //查找特定的key
+  searchTree.prototype.search = function (key) {
+    //1.获取根节点
+    let node = this.root;
+
+    //2.循环搜索key
+    while (node != null) {
+      if (key < node.key) {
+        //小于根(父)节点就往左边找
+        node = node.left;
+        //大于根(父)节点就往右边找
+      } else if (key > node.key) {
+        node = node.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  //四.删除节点
+  searchTree.prototype.remove = function (key) {
+    /*------------------------------1.寻找要删除的节点---------------------------------*/
+    //1.1.定义变量current保存删除的节点，parent保存它的父节点。isLeftChild保存current是否为parent的左节点
+    let current = this.root;
+    let parent = null;
+    let isLeftChild = true;
+
+    //1.2.开始寻找删除的节点
+    while (current.key != key) {
+      parent = current;
+      // 小于则往左查找
+      if (key < current.key) {
+        isLeftChild = true;
+        current = current.left;
+      } else {
+        isLeftChild = false;
+        current = current.right;
+      }
+      //找到最后依然没有找到相等的节点
+      if (current == null) {
+        return false;
+      }
+    }
+    //结束while循环后：current.key = key
+
+    /*------------------------------2.根据对应情况删除节点------------------------------*/
+    //情况1：删除的是叶子节点(没有子节点)
+    if (current.left == null && current.right == null) {
+      if (current == this.root) {
+        this.root = null;
+      } else if (isLeftChild) {
+        parent.left = null;
+      } else {
+        parent.right = null;
+      }
+    }
+    //情况2：删除的节点有一个子节点
+    //当current存在左子节点时
+    else if (current.right == null) {
+      if (current == this.root) {
+        this.root = current.left;
+      } else if (isLeftChild) {
+        parent.left = current.left;
+      } else {
+        parent.right = current.left;
+      }
+      //当current存在右子节点时
+    } else if (current.left == null) {
+      if (current == this.root) {
+        this.root = current.right;
+      } else if (isLeftChild) {
+        parent.left = current.right;
+      } else {
+        parent.right = current.right;
+      }
+    }
+    //情况3：删除的节点有两个子节点
+    else {
+      //1.获取后继节点
+      let successor = this.getSuccessor(current);
+
+      //2.判断是否根节点
+      if (current == this.root) {
+        this.root = successor;
+      } else if (isLeftChild) {
+        parent.left = successor;
+      } else {
+        parent.right = successor;
+      }
+
+      //3.将后继的左子节点改为被删除节点的左子节点
+      successor.left = current.left;
+    }
+  };
+
+  searchTree.prototype.preOrderNode = function (node, handler) {
+    if (node != null) {
+      this.preOrderNode(node.left, handler);
+      handler(node.key);
+      this.preOrderNode(node.right, handler);
+    }
+  };
+  searchTree.prototype.MiddleNode = function (node, handler) {
+    if (node != null) {
+      this.preOrderNode(node.left, handler);
+      handler(node.key); // 中序
+      this.preOrderNode(node.right, handler);
+    }
+  };
+  searchTree.prototype.AfterNode = function (node, handler) {
+    if (node != null) {
+      this.preOrderNode(node.left, handler);
+      this.preOrderNode(node.right, handler);
+      handler(node.key); // 后序
+    }
+  };
 }
+
+let s = new searchTree();
+s.inster(10);
+s.inster(8);
+s.inster(7);
+s.inster(6);
+s.inster(12);
+s.inster(14);
+
+s.xianxu(function (val) {
+  console.log(val);
+});
+console.log(s);
--- README.md
+++ README.md
@@ -2,6 +2,14 @@
  * @Date: 2020-08-25 10:27:37
 -->
 
-## 完成功能
+## JavaScript 各种原生方法实现
 
-1. call 方法实现
+傻瓜式,简单易懂
+
+### 关注微信公众号`码不停息`,解锁更多优质文章
+
+![](https://mmbiz.qpic.cn/mmbiz_jpg/a3Xykic19Nia4Yw8GdEAud7CEtbljx99YHq4p6hkSwK5NrFTVTia3AXHBzn4PZtof93jDdyj8pmILepOh012NMjWw/0?wx_fmt=jpeg)
+
+### 加群交流
+
+![](http://www.h5love.cn/01.png)
